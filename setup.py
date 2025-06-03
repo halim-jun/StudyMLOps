3aac98dea3b995f04bbb455f77ae15c685e68529
HaLim <haileyhalimj@gmail.com>
2025-06-03 11:48:20 +0900

    updated setup.py and pyproject.toml

--- setup.py
+++ setup.py
@@ -12,6 +12,13 @@ setup(
         "pandas>=2.2.3,<3.0.0",
         "matplotlib>=3.10.3,<4.0.0",
         "kagglehub>=0.2.0",
+        "scikit-learn>=1.6.1,<2.0.0",
+        "joblib>=1.5.1,<2.0.0",
+        "numpy>=2.2.6,<3.0.0",
+        "pandas>=2.2.3,<3.0.0",
+        "matplotlib>=3.10.3,<4.0.0",
+        "kagglehub>=0.2.0",
+        "scikit-learn>=1.6.1,<2.0.0",
     ],
     readme="README.md",
 )
